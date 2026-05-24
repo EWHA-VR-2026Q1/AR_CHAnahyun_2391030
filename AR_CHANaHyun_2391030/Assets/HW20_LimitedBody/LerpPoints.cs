48cdf18ec93d44f3c78ff6ba655c9dfd5f4693a6
cnh0922-hash <cnh0922@ewhain.net>
2026-05-24 20:33:32 +0900

    HW20

--- AR_CHANaHyun_2391030/Assets/HW20_LimitedBody/LerpPoints.cs
+++ AR_CHANaHyun_2391030/Assets/HW20_LimitedBody/LerpPoints.cs
@@ -5,8 +5,8 @@ public class LerpPoints : MonoBehaviour, IInteractable
 {
     private Camera mainCamera;
     private bool isDragging = false;
+    private float zDistance;
     private Vector3 offset;
-    private Plane dragPlane;
 
     private void Awake()
     {
@@ -18,18 +18,16 @@ public class LerpPoints : MonoBehaviour, IInteractable
         var pointer = Pointer.current;
         if (pointer == null) return;
 
+        Vector2 screenPos = pointer.position.ReadValue();
         isDragging = true;
 
-        dragPlane = new Plane(transform.up, transform.position);
+        zDistance = mainCamera.WorldToScreenPoint(transform.position).z;
+        Vector3 clickWorldPos = mainCamera.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, zDistance));
 
-        Vector2 screenPos = pointer.position.ReadValue();
-        Ray ray = mainCamera.ScreenPointToRay(screenPos);
-
-        if (dragPlane.Raycast(ray, out float enter))
-        {
-            Vector3 clickWorldPos = ray.GetPoint(enter);
+        if (transform.parent != null)
+            offset = transform.localPosition - transform.parent.InverseTransformPoint(clickWorldPos);
+        else
             offset = transform.position - clickWorldPos;
-        }
 
         Renderer rend = GetComponentInChildren<Renderer>();
         if (rend != null) rend.material.color = Color.red;
@@ -50,18 +48,13 @@ public class LerpPoints : MonoBehaviour, IInteractable
         if (isDragging)
         {
             Vector2 screenPos = pointer.position.ReadValue();
-            Ray ray = mainCamera.ScreenPointToRay(screenPos);
+            Vector3 mousePoint = new Vector3(screenPos.x, screenPos.y, zDistance);
+            Vector3 newPosition = mainCamera.ScreenToWorldPoint(mousePoint);
 
-            if (dragPlane.Raycast(ray, out float enter))
-            {
-                Vector3 targetPos = ray.GetPoint(enter) + offset;
-
-                transform.position = targetPos;
-            }
+            transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
         }
     }
 
-    // --- 인터페이스 필수 구현부 ---
     public void OnEnter() { }
     public void OnExit() { }
     public void OnStay() { }
